af69e5e62101f51d649dd78bff8a5952cb547dcc
lucianesd <lusdamaso@gmail.com>
2025-05-09 11:25:33 -0300

    configuracion github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  site: "https://lucianesd.github.io",
+  site: "https://lucianesd.github.io/",
   base: "Portafolio-LuDamaso",
 });
